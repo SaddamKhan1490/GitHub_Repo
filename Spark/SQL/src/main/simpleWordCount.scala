62e2cba6e3b1bea77ee2fae896d9ff786673f8a8
Vchavan91 <vidyachavan091@gmail.com>
2016-02-07 15:36:44 +0530

    Update simpleWordCount.scala

--- Spark/SQL/src/main/simpleWordCount.scala
+++ Spark/SQL/src/main/simpleWordCount.scala
@@ -15,7 +15,7 @@ object WordCount {
       val sc = new SparkContext(conf)
       println("Start")
       //If You are in Spark Cluster the povide HDFS Path instead of Local Path
-      val textFile = sc.textFile("C:/Users/Lenovo/Desktop/Input_Dataset/Sample") 
+      val textFile = sc.textFile("C:/Users/Lenovo/Desktop/Input_Dataset/Sample.txt") 
       val counts = textFile.flatMap(line => line.split(" "))
                  .map(word => (word, 1))
                  .reduceByKey(_ + _)
