50cb73fbffb96d0c54fe710efc7f9bf900933355
Vchavan91 <vidyachavan091@gmail.com>
2016-02-07 22:24:14 +0530

    Create sparkJobServer.scala

---
+++ Spark/SQL/src/main/sparkJobServer.scala
@@ -0,0 +1,34 @@
+/**
+ * Date :-  02/07/16.
+ * Author :- Vidya
+ */
+
+import org.apache.log4j.{Level, Logger}
+import com.typesafe.config.Config
+import org.apache.spark.SparkContext, SparkContext._
+import org.apache.spark.rdd.RDD
+import org.apache.spark._
+
+
+println("Start...")
+
+// Extending SparkJob trait 
+trait UsersSparkJob extends spark.jobserver.SparkJob with spark.jobserver.NamedRddSupport with UsersRDDBuilder {
+  val rddName = "users"
+
+// Implementing validate Function of SparkJob for doing sanity check of submitted jobs
+  def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = spark.jobserver.SparkJobValid
+}
+
+// Implementing runJob Function of SparkJob for running the jobs
+object GetOrCreateUsers extends UsersSparkJob {
+  override def runJob(sc: SparkContext, config: Config) = {
+    val users: RDD[(Reputation, User)] = namedRdds.getOrElseCreate(
+      rddName,
+      build(sc))
+    users.take(5)
+  }
+}
+println("End...")
+
+}
