b77533e59df5019ce4d61bd4a6faa91285b641eb
Vchavan91 <vidyachavan091@gmail.com>
2016-02-10 12:52:11 +0530

    Update SparkSQLwithSocketStreaming.scala

--- Spark/Streaming/src/main/SparkSQLwithSocketStreaming.scala
+++ Spark/Streaming/src/main/SparkSQLwithSocketStreaming.scala
@@ -11,7 +11,7 @@
   import org.apache.spark.streaming.{Seconds, StreamingContext, Time}
   import org.apache.spark.util.IntParam
   
-object SqlNetworkWordCount {
+object sparkSQLwithSocketStreaming {
     // Checking the authentication credentials 
   def main(args: Array[String]) {
     if (args.length < 2) {
@@ -22,11 +22,12 @@ object SqlNetworkWordCount {
     StreamingExamples.setStreamingLogLevels()
 
     // Creating the context with a 2 second batch size
-    val sparkConf = new SparkConf().setAppName("SqlNetworkWordCount")
+    val sparkConf = new SparkConf().setAppName("sparkSQLwithSocketStreaming")
     val ssc = new StreamingContext(sparkConf, Seconds(2))
     
     /* Configuring & Creating spark streaming context to receive dstreams from <hostname> <port>
      * Where, <hostname> and <port> describe the TCP server that Spark Streaming would connect to receive data.
+     */
     val lines = ssc.socketTextStream(args(0), args(1).toInt, StorageLevel.MEMORY_AND_DISK_SER)
     val words = lines.flatMap(_.split(" "))
     val pairs = words.map(word => (word, 1))
