fe28a99466ae3a71f1c677e2a6438284785d19e7
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-01-11 17:30:16 +0530

    Update 1.1_Hadoop_Directory_Structure.sh

--- Batch-Ingestion-Processing/1.1_Hadoop_Directory_Structure.sh
+++ Batch-Ingestion-Processing/1.1_Hadoop_Directory_Structure.sh
@@ -1,11 +1,11 @@
-Raw :-
+#Raw :-
 hdfs dfs -mkdir /user/data/raw/ or hadoop fs -mkdir /user/data/raw/
 
-Decomposed :-
+#Decomposed :-
 hdfs dfs -mkdir /user/data/decomposed/ or hadoop fs -mkdir /user/data/decomposed/
 
-Modelled :-
+#Modelled :-
 hdfs dfs -mkdir /user/data/modelled/ or hadoop fs -mkdir /user/data/modelled/
 
-Schema :-
+#Schema :-
 hdfs dfs -mkdir /user/data/schema/ or hadoop fs -mkdir /user/data/schema/
