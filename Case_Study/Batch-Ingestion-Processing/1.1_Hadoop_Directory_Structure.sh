66744d7ce1a2d270f08c8d42df740385dde9e484
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-01-12 17:06:13 +0530

    1.1_Hadoop_Directory_Structure.sh

---
+++ Case_Study/Batch-Ingestion-Processing/1.1_Hadoop_Directory_Structure.sh
@@ -0,0 +1,15 @@
+#Raw :-
+hdfs dfs -mkdir /user/data/raw/ 
+hadoop fs -mkdir /user/data/raw/
+
+#Decomposed :-
+hdfs dfs -mkdir /user/data/decomposed/
+hadoop fs -mkdir /user/data/decomposed/
+
+#Modelled :-
+hdfs dfs -mkdir /user/data/modelled/
+hadoop fs -mkdir /user/data/modelled/
+
+#Schema :-
+hdfs dfs -mkdir /user/data/schema/ 
+hadoop fs -mkdir /user/data/schema/
