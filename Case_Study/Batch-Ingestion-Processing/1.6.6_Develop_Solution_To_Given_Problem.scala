4671818dc5e8fcaeb11bd39be35d07b1e515acc9
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-01-13 14:16:26 +0530

    Create 1.6.6_Develop_Solution_To_Given_Problem.scala

---
+++ Case_Study/Batch-Ingestion-Processing/1.6.6_Develop_Solution_To_Given_Problem.scala
@@ -0,0 +1,14 @@
+6>	How well does weather predict plane delays?
+
+Weather has no influence on flight if : (([avg_absolute_delay_with_weather_delay - avg_absolute_delay_without_weather_delay]) - ([avg_absolute_weather_delay])) === 0
+Weather is increasing the flight delay if : (([avg_absolute_delay_with_weather_delay - avg_absolute_delay_without_weather_delay]) - ([avg_absolute_weather_delay])) >== 0
+Weather is decreasing the flight delay if : (([avg_absolute_delay_with_weather_delay - avg_absolute_delay_without_weather_delay]) - ([avg_absolute_weather_delay])) <== 0
+
+Note : Here we are considering ideal scenario i.e. wind is neither increasing the speed of plane (i.e. upStream) nor decreasing speed of plane (i.e. downStream)
+=====================================================================================================================================================================
+                                ##### #   #  #
+                                #     ##  #  # #
+                                ###   # # #  #  #
+                                #     #  ##  # #
+                                ##### #   #  #
+=====================================================================================================================================================================
