0ff4d962ea8fd5d63921063f31170079bb7f2c0a
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-05-26 20:27:58 +0400

    Create CourseApiApp.java

---
+++ RESTful_WebService/SpringBoot/DemoRestApi/src/main/java/com.webservice_tutorial.springbootstarter/CourseApiApp.java
@@ -0,0 +1,20 @@
+/**
+  * Created by Saddam Khan on 5/24/2018.
+  */
+
+package com.webservice_tutorial.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SpringApplication.run(CourseApiApp.class,args);
+				
+	}
+
+}
