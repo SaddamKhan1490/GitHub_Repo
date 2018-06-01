a5692c61280d86e81ee1a164f3ba19d1ad0b8682
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-01 10:24:16 +0400

    Create TopicRepository.java

---
+++ RESTful_WebService/SpringBoot/DemoRestApi_JPA/src/main/java/com.example.topic.model-dao-service-controller/TopicRepository.java
@@ -0,0 +1,13 @@
+
+/**
+  * Created by Saddam Khan on 05/31/2018.
+  */
+ 
+package com.example.topic.model-dao-service-controller;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository <Topic, String>{
+
+}
