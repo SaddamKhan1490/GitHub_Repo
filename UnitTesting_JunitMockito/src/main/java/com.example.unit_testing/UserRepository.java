4727490c804096d8879b74e029986d0d4fcfd845
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-06 18:40:42 +0400

    Create UserRepository.java

---
+++ UnitTesting_JunitMockito/src/main/java/com.example.unit_testing/UserRepository.java
@@ -0,0 +1,18 @@
+/**
+  * Created by Saddam on 05/31/2018.
+  */
+ 
+package com.example.unit_testing;
+
+import java.util.List;
+
+import org.apache.tomcat.jni.User;
+
+public class UserRepository {
+
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
