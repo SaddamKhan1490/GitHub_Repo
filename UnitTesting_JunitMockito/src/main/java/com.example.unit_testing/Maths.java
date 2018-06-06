c85aaed6c7171fa7bfc80a2db99c72a36e3dc507
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-06 18:36:35 +0400

    Create Maths.java

---
+++ UnitTesting_JunitMockito/src/main/java/com.example.unit_testing/Maths.java
@@ -0,0 +1,33 @@
+/**
+  * Created by Saddam on 05/31/2018.
+  */
+ 
+package com.example.unit_testing;
+
+public class Maths {
+	
+    public int add(int first_number, int second_number) {
+
+      return (first_number+second_number);
+
+    }
+
+    public int sub(int first_number, int second_number) {
+
+      return (first_number-second_number);
+
+    }
+
+    public int mul(int first_number, int second_number) {
+
+      return (first_number*second_number);
+
+    }
+
+    public int div(int first_number, int second_number) {
+
+      return (first_number/second_number);
+
+    }
+
+}
