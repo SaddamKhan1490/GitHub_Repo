2badd3ef5706410769cb56ceb18ac6f34e64e743
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-06 18:39:57 +0400

    Create StubInterface.java

---
+++ UnitTesting_JunitMockito/src/main/java/com.example.unit_testing/StubInterface.java
@@ -0,0 +1,13 @@
+/**
+  * Created by Saddam on 05/31/2018.
+  */
+ 
+package com.example.unit_testing;
+
+import java.util.List;
+
+public interface StubInterface {
+	
+	public List<String> retrieve(String user);
+	
+}
