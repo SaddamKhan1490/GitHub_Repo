b7a4e5bdeed05e679ac2592127a24a3eff54d985
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-07 15:05:05 +0400

    Create SpringSwaggerApplication.java

---
+++ RESTful_WebService/SpringBoot/SpringSwagger/src/main/java/com.example.SpringSwagger/SpringSwaggerApplication.java
@@ -0,0 +1,17 @@
+/**
+  * Created by Saddam Khan on 5/14/2018.
+  */
+ 
+ 
+package com.example.SpringSwagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwaggerApplication.class, args);
+	}
+}
