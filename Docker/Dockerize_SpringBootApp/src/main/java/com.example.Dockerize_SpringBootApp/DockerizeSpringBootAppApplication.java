0b584b36f7c6d54f1c6bbcf69acadbf1e85ff810
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-08 20:09:10 +0400

    Create DockerizeSpringBootAppApplication.java

---
+++ Docker/Dockerize_SpringBootApp/src/main/java/com.example.Dockerize_SpringBootApp/DockerizeSpringBootAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.Dockerize_SpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeSpringBootAppApplication.class, args);
+	}
+}
