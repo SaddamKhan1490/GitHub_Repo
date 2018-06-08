2d88902578166a54978f0f3af540e42b36f4d829
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-08 20:08:00 +0400

    HelloResource.java

---
+++ Docker/Dockerize_SpringBootApp/src/main/java/com.example.resource/HelloResource.java
@@ -0,0 +1,15 @@
+package com.example.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController                                   // @RestController annotation is used to associate REST controller definition with HelloResource class 
+@RequestMapping("/rest/docker/hello")             // @RequestMapping annotation is used to specify REST request URL for our application
+public class HelloResource {
+
+	@GetMapping                                     // @GetMapping annotation is used to generate HTTP GET response for given request
+	public String hello() {
+		return "Hello World";
+	}
+}
