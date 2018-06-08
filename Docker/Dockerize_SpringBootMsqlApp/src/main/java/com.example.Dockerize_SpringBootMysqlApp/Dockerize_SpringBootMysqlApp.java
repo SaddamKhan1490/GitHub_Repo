2496bf6d0b605f5c261092a51b549057e40944e5
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-08 18:25:41 +0400

    Create Dockerize_SpringBootMysqlApp.java

---
+++ Docker/Dockerize_SpringBootMsqlApp/src/main/java/com.example.Dockerize_SpringBootMysqlApp/Dockerize_SpringBootMysqlApp.java
@@ -0,0 +1,16 @@
+/**
+  * Created by Saddam on 04/31/2018
+  */
+  
+package com.example.Dockerize_SpringBootMysqlApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeSpringBootMysqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeSpringBootMysqlAppApplication.class, args);
+	}
+}
