55c301cce2c9aa2ca9fd25dea9e9134e9808d038
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-12 18:49:17 +0400

    Create SpringSecurityWithLDAP.java

---
+++ Spring_Security/SpringSecurityWithLDAP/src/main/java/com.example.SpringSecurityWithLDAP/SpringSecurityWithLDAP.java
@@ -0,0 +1,16 @@
+/**
+  * Created by Saddam on 05/24/2018
+  */
+  
+package com.example.SpringSecurityWithLDAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithLdapApplication.class, args);
+	}
+}
