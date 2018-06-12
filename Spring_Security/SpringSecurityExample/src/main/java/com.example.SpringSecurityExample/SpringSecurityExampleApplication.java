836782409a8d0b0d4c6fc44d8317b59dcca94e40
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-12 15:34:38 +0400

    Create SpringSecurityExampleApplication.java

---
+++ Spring_Security/SpringSecurityExample/src/main/java/com.example.SpringSecurityExample/SpringSecurityExampleApplication.java
@@ -0,0 +1,16 @@
+/**
+  * Created by Saddam on 05/14/2018
+  */
+  
+  package com.example.SpringSecurityExample;
+
+  import org.springframework.boot.SpringApplication;
+  import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+  @SpringBootApplication
+  public class SpringSecurityExampleApplication {
+
+    public static void main(String[] args) {
+      SpringApplication.run(SpringSecurityExampleApplication.class, args);
+    }
+  }
