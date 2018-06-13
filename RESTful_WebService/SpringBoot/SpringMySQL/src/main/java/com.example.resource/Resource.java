3eb5489b9093341e2e9d9966461c7166a52046b6
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-14 00:47:59 +0400

    Create Resource.java

---
+++ RESTful_WebService/SpringBoot/SpringMySQL/src/main/java/com.example.resource/Resource.java
@@ -0,0 +1,47 @@
+/**
+  * Created by Saddam on 04/12/2018
+  */
+  
+package com.example.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Users;
+import com.example.repository.UserRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/all")
+public class Resource {
+
+    private UserRepository usersRepository;
+
+    public Resource(UserRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+
+    @GetMapping("/")
+    public List<Users> all() {
+
+
+        return usersRepository.findAll();
+    }
+
+
+    @GetMapping("/create")
+    public List<Users> users() {
+        Users users = new Users();
+        users.setId(1);
+        users.setName("Sam");
+        users.setSalary(3400);
+        users.setTeamName("Development");
+
+        usersRepository.save(users);
+
+        return usersRepository.findAll();
+    }
+}
