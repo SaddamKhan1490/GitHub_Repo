3306eb9769d4e2fcc84cce138c12e60decdb97c3
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-14 00:43:37 +0400

    Create SpringMySQLApplication.java

---
+++ RESTful_WebService/SpringBoot/SpringMySQL/src/main/java/com.example.SpringMySQL/SpringMySQLApplication.java
@@ -0,0 +1,16 @@
+/**
+  * Created by Saddam on 04/12/2018
+  */
+  
+package com.example.SpringMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMySQLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeSpringBootMysqlAppApplication.class, args);
+	}
+}
