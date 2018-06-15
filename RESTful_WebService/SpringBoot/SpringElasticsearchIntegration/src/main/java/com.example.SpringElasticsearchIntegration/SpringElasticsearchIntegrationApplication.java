c8fcaaf9b26be1e76d55a5f4f9353ec104d72186
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-15 23:42:04 +0400

    Create SpringElasticsearchIntegrationApplication.java

---
+++ RESTful_WebService/SpringBoot/SpringElasticsearchIntegration/src/main/java/com.example.SpringElasticsearchIntegration/SpringElasticsearchIntegrationApplication.java
@@ -0,0 +1,16 @@
+/**
+  * Created by Saddam on 04/18/2018
+  */
+  
+package com.example.SpringElasticsearchIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticsearchIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticsearchIntegrationApplication.class, args);
+	}
+}
