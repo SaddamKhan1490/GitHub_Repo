27a678941aebecd5cc6b82d37fc74af863ebcce1
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-16 21:54:27 +0400

    Create SpringBootGraphqlExampleApplication.java

---
+++ GraphQL/src/main/java/com.example.SpringBootGraphqlExample/SpringBootGraphqlExampleApplication.java
@@ -0,0 +1,17 @@
+
+/**
+  * Created by Saddam on 05/04/2018
+  */
+  
+package com.example.SpringBootGraphqlExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphqlExampleApplication.class, args);
+	}
+}
