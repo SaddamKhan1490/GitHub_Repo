3d9e8055ee8c202a5482841b3d10e3a2fb1aa3a2
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-16 21:56:54 +0400

    Create Book.java

---
+++ GraphQL/src/main/java/com.example.model/Book.java
@@ -0,0 +1,30 @@
+/**
+  * Created by Saddam on 05/04/2018
+  */
+  
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+@Entity
+public class Book {
+
+    @Id
+    private String isn;
+    private String title;
+    private String publisher;
+    private String[] authors;
+    private String publishedDate;
+}
