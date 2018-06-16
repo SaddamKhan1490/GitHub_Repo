d97535775a468d1bd3303285df5982b9f94589c3
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-16 22:10:03 +0400

    Create BookDataFetcher.java

---
+++ GraphQL/src/main/java/com.example.service.datafetcher/BookDataFetcher.java
@@ -0,0 +1,27 @@
+/**
+  * Created by Saddam on 05/04/2018
+  */
+  
+package com.example.service.datafetcher;
+
+import com.techprimers.graphql.springbootgrapqlexample.model.Book;
+import com.techprimers.graphql.springbootgrapqlexample.repository.BookRepository;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookDataFetcher implements DataFetcher<Book>{
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Override
+    public Book get(DataFetchingEnvironment dataFetchingEnvironment) {
+
+        String isn = dataFetchingEnvironment.getArgument("id");
+
+        return bookRepository.findOne(isn);
+    }
+}
