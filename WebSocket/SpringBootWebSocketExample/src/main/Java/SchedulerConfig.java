7f3d2c5825f085859babd2f0d124dbccc01eb96c
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-17 20:56:52 +0400

    Create SchedulerConfig.java

---
+++ WebSocket/SpringBootWebSocketExample/src/main/Java/SchedulerConfig.java
@@ -0,0 +1,25 @@
+/**
+  * Created by Saddam on 05/07/2018
+  */
+  
+package com.example.config;
+
+import com.example.model.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@EnableScheduling
+@Configuration
+public class SchedulerConfig {
+
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @Scheduled(fixedDelay = 3000)
+    public void sendAdhocMessages() {
+        template.convertAndSend("/topic/user", new UserResponse("Fixed Delay Scheduler"));
+    }
+}
