be245b3febb7939200856436c33cc1c17af11032
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-17 21:00:57 +0400

    Create UserResponse.java

---
+++ WebSocket/SpringBootWebSocketExample/src/main/Java/com.example.model/UserResponse.java
@@ -0,0 +1,20 @@
+/**
+  * Created by Saddam on 05/07/2018
+  */
+  
+package com.example.model;
+
+public class UserResponse {
+    String content;
+
+    public UserResponse() {
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public UserResponse(String content) {
+        this.content = content;
+    }
+}
