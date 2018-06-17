caa16d851777d5737d5412bad4a2e631825494de
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-17 20:52:32 +0400

    Create SpringBootWebSocketExampleApplication.java

--- WebSocket/SpringBootWebSocketExample/src/main/Java/com.example.SpringBootWebSocketExample/SpringBootWebSocketExampleApplication.java
+++ WebSocket/SpringBootWebSocketExample/src/main/Java/com.example.SpringBootWebSocketExample/SpringBootWebSocketExampleApplication.java
@@ -8,9 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWebsocketExampleApplication {
+public class SpringBootWebSocketExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebsocketExampleApplication.class, args);
+		SpringApplication.run(SpringBootWebSocketExampleApplication.class, args);
 	}
 }
