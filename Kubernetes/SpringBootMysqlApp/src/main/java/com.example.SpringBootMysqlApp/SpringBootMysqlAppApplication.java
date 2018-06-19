98d357a803c6c9d95306031e4b28e980c59329a9
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-19 16:33:01 +0400

    Create SpringBootMysqlAppApplication.java

---
+++ Kubernetes/SpringBootMysqlApp/src/main/java/com.example.SpringBootMysqlApp/SpringBootMysqlAppApplication.java
@@ -0,0 +1,16 @@
+/**
+  * Created by Saddam on 05/31/2018
+  */
+  
+package com.example.SpringBootMysqlApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlAppApplication.class, args);
+	}
+}
