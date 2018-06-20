d95e9cae75652afdde001acd5ab0c69c277b1d44
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-20 09:53:11 +0400

    Create ProductToProductForm.java

---
+++ RESTful_WebService/SpringBoot/SpringCassandraIntegration/src/main/java/com.example.converters/ProductToProductForm.java
@@ -0,0 +1,25 @@
+/**
+  * Created by Saddam on 04/28/2018
+  */
+  
+  
+package com.example.converters;
+
+import com.example.document.Product;
+import com.example.model.ProductForm;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+public class ProductToProductForm implements Converter<Product, ProductForm> {
+    @Override
+    public ProductForm convert(Product product) {
+        ProductForm productForm = new ProductForm();
+        productForm.setId(product.getId());
+        productForm.setDescription(product.getDescription());
+        productForm.setPrice(product.getPrice());
+        productForm.setImageUrl(product.getImageUrl());
+        return productForm;
+    }
+}
