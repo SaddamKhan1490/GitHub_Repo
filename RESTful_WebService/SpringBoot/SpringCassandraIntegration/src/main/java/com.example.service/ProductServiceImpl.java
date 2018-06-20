e72983783585f7c86d852833398f4285c19e35f3
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-20 10:03:40 +0400

    Create ProductServiceImpl.java

---
+++ RESTful_WebService/SpringBoot/SpringCassandraIntegration/src/main/java/com.example.service/ProductServiceImpl.java
@@ -0,0 +1,64 @@
+/**
+  * Created by Saddam on 04/28/2018
+  */
+  
+  
+package com.example.service;
+
+import com.example.document.Product;
+import com.example.model.ProductForm;
+import com.example.converters.ProductFormToProduct;
+import guru.springframework.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+    private ProductFormToProduct productFormToProduct;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository, ProductFormToProduct productFormToProduct) {
+        this.productRepository = productRepository;
+        this.productFormToProduct = productFormToProduct;
+    }
+
+
+    @Override
+    public List<Product> listAll() {
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add); //fun with Java 8
+        return products;
+    }
+
+    @Override
+    public Product getById(UUID id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Product saveOrUpdate(Product product) {
+        productRepository.save(product);
+        return product;
+    }
+
+    @Override
+    public void delete(UUID id) {
+        productRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Product saveOrUpdateProductForm(ProductForm productForm) {
+        Product savedProduct = saveOrUpdate(productFormToProduct.convert(productForm));
+
+        System.out.println("Saved Product Id: " + savedProduct.getId());
+        return savedProduct;
+    }
+}
