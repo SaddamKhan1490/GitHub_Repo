93c6038863f0bcf9f63c37fcc28a9ab26ac7b944
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-20 09:45:29 +0400

    Create Product.java

---
+++ RESTful_WebService/SpringBoot/SpringCassandraIntegration/src/main/java/com.example.document/Product.java
@@ -0,0 +1,63 @@
+/**
+  * Created by Saddam on 04/28/2018
+  */
+
+
+package com.example.document;
+
+import com.datastax.driver.core.DataType;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+
+@Table("products")
+public class Product implements Serializable{
+
+    @PrimaryKey
+    @CassandraType(type = DataType.Name.UUID)
+    private UUID id;
+    private String description;
+    private BigDecimal price;
+    private String imageUrl;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Product() {
+        id = UUID.randomUUID();
+    }
+}
