6ca2fcc727335fcebb8288c598b71ef092938c11
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-20 10:06:23 +0400

    Create SpringCassandraIntegrationApplication.java

---
+++ RESTful_WebService/SpringBoot/SpringCassandraIntegration/src/main/java/com.example.SpringCassandraIntegration/SpringCassandraIntegrationApplication.java
@@ -0,0 +1,18 @@
+/**
+  * Created by Saddam on 04/28/2018
+  */
+  
+  
+package com.example.SpringCassandraIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringCassandraIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCassandraIntegrationApplication.class, args);
+	}
+}
