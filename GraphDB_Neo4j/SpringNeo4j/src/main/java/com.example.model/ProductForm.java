6481d333e4a22ebc27a959e1a9f1263cc7710b97
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-21 23:44:04 +0400

    Create ProductForm.java

---
+++ GraphDB_Neo4j/SpringNeo4j/src/main/java/com.example.model/ProductForm.java
@@ -0,0 +1,48 @@
+/**
+  * Created by Saddam on 04/28/2018
+  */
+  
+  
+package com.example.model;
+
+import java.math.BigDecimal;
+
+
+public class ProductForm {
+    private Long id;
+    private String description;
+    private BigDecimal price;
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
