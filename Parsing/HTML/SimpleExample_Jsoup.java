a7c91336f888ef48f2306c8c4cc0436232022d6b
SaddamKhan1490 <saddamhkhan1403@gmail.com>
2018-06-26 21:48:23 +0400

    Create SimpleExample_Jsoup.java

--- Parsing/HTML/SimpleExample_Jsoup.java
+++ Parsing/HTML/SimpleExample_Jsoup.java
@@ -23,40 +23,40 @@ public class HTML_Parser {
 				  + "<body><p>Parsed HTML into a doc.</p>"
 				  + "<a>Parsed HTML into a doc.</a></body></html>";
 	
-				Document doc_hardcoded = Jsoup.parse(html);
-				Elements anchor_content = doc_hardcoded.select("a");
-				Elements paragraph_content = doc_hardcoded.select("p");
-				Element head_content = doc_hardcoded.select("head").first();
+			Document doc_hardcoded = Jsoup.parse(html);
+			Elements anchor_content = doc_hardcoded.select("a");
+			Elements paragraph_content = doc_hardcoded.select("p");
+			Element head_content = doc_hardcoded.select("head").first();
 				
-			    System.out.println("Hardcoded_Anchor:-"+ "\n" + anchor_content);
-			    System.out.println("Hardcoded_Paragraph:-"+ "\n" + paragraph_content);
-			    System.out.println("Hardcoded_Head:-"+ "\n" + head_content);
+			System.out.println("Hardcoded_Anchor:-"+ "\n" + anchor_content);
+			System.out.println("Hardcoded_Paragraph:-"+ "\n" + paragraph_content);
+			System.out.println("Hardcoded_Head:-"+ "\n" + head_content);
 		
 			    
 		// Read HTML_File and Parse using Jsoup
 	    Validate.isTrue(args.length == 1, "usage: supply file to fetch");
 	    String file = args[0];
 	    System.out.println(file); 
-		StringBuilder contentBuilder = new StringBuilder();
-			    try {
+	    StringBuilder contentBuilder = new StringBuilder();
+			try {
 			        BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\Lenovo\\Desktop\\mypage.html"));
 			        String str;
-			        while ((str = in.readLine()) != null) {
-			            contentBuilder.append(str);
-			        }
+			     while ((str = in.readLine()) != null) {
+			         contentBuilder.append(str);
+			     }
 			        in.close();
-			    } catch (IOException e) {
-			    }
-			    String html_file_content = contentBuilder.toString();
+			} catch (IOException e) {
+			}
+			String html_file_content = contentBuilder.toString();
 			    
-			    Document doc_html_file = Jsoup.parse(html_file_content);
-				Elements anchor_html_file = doc_html_file.select("a");
-				Elements paragraph_html_file= doc_html_file.select("p");
-				Element head_html_file = doc_html_file.select("head").first();
+			Document doc_html_file = Jsoup.parse(html_file_content);
+			Elements anchor_html_file = doc_html_file.select("a");
+			Elements paragraph_html_file= doc_html_file.select("p");
+			Element head_html_file = doc_html_file.select("head").first();
 				
-			    System.out.println("File_Anchor:-"+ "\n" + anchor_html_file);
-			    System.out.println("File_Paragraph:-"+ "\n" + paragraph_html_file);
-			    System.out.println("File_Head:-"+  "\n" +head_html_file);
+			System.out.println("File_Anchor:-"+ "\n" + anchor_html_file);
+			System.out.println("File_Paragraph:-"+ "\n" + paragraph_html_file);
+			System.out.println("File_Head:-"+  "\n" +head_html_file);
 		
 			    
 		// Read HTML_URL and Parse using Jsoup
